45af4258bba1c67d09fcaf4eddfe9929eff1dd61
Bruno Cabral <cabral.brc@gmail.com>
2024-06-04 22:42:44 -0700

    draw moving rectangle

--- main.cpp
+++ main.cpp
@@ -2,12 +2,19 @@
 
 int main()
 {
+    int posX = 0;
+
     InitWindow(800, 450, "raylib - basic window");
+
+    SetTargetFPS(144);
+
     while (!WindowShouldClose())
     {
+        posX++;
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawRectangle(posX, 100, 100, 100, BLUE);
         EndDrawing();
     }
     CloseWindow();
