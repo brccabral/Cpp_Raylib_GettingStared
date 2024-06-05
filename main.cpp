ccb7c0ac3f50328a4689c148c9e914ae35298ba0
Bruno Cabral <cabral.brc@gmail.com>
2024-06-04 23:13:10 -0700

    play sound

--- main.cpp
+++ main.cpp
@@ -3,15 +3,24 @@
 int main()
 {
     Texture2D sprite; // need to load after InitWindow because InitWindow loads the graphics libraries
+    Sound sound; // need to InitAudioDevice
 
     InitWindow(800, 450, "raylib - basic window");
     SetTargetFPS(144);
+    InitAudioDevice();
 
     sprite = LoadTexture("resources/sprite.png");
     float posX = -sprite.width;
 
+    sound = LoadSound("resources/sound.ogg");
+
     while (!WindowShouldClose())
     {
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        {
+            PlaySound(sound);
+        }
+
         posX += GetFrameTime() * 300.0f;
         if (posX > 800)
         {
@@ -25,6 +34,7 @@ int main()
 
         EndDrawing();
     }
+    CloseAudioDevice();
     CloseWindow();
     return 0;
 }
